bfd3fc70870c87c96b7f49528f9b97f8171c4e25
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-26 20:26:23 -0600

    Create 9.5_Fibonacci.py

---
+++ 9.5_Fibonacci.py
@@ -0,0 +1,9 @@
+'''
+FIBONACCI
+---------
+In mathematics, the Fibonacci numbers are the numbers in the following integer sequence, 
+called the Fibonacci sequence, and characterized by the fact that every number after the
+first two is the sum of the two preceding ones:1,1,2,3,5,8,13,21,34,55,89,144
+Write a function called fibonacci() that will print up to a maximum of the first 100 numbers
+in the Fibonacci sequence. Pass the number into the function.
+'''
