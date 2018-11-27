b435d24effda242af538c4ab2c7b46cae8cfea88
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-26 20:32:20 -0600

    Update 9.5_Fibonacci.py

--- 9.5_Fibonacci.py
+++ 9.5_Fibonacci.py
@@ -6,4 +6,7 @@ called the Fibonacci sequence, and characterized by the fact that every number a
 first two is the sum of the two preceding ones:1,1,2,3,5,8,13,21,34,55,89,144
 Write a function called fibonacci() that will print up to a maximum of the first 100 numbers
 in the Fibonacci sequence. Pass the number into the function.
+
+Just to do a quick review of text formatting in the last chapter, make the list of numbers
+right-justified with commas.
 '''
