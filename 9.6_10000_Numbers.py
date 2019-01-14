e63781b9500ffa27585fd030426cd6187be94746
Marc Hermon <hermonm@urbandaleschools.com>
2019-01-14 08:31:49 -0600

    lines of code

--- 9.6_10000_Numbers.py
+++ 9.6_10000_Numbers.py
@@ -74,9 +74,9 @@ OUTPUT
 
 '''
 Now that the functions have been created, use them all in a main program that will:
-1.) Create a list of 10,000 random numbers from 1 to 6.
-2.) Print the count of 1 through 6. (For example, "There are 1361 amount of 2s")
-3.) Print the average of all 10,000 random numbers. (Make sure it's a float)
+1.) Create a list of 10,000 random numbers from 1 to 6. (1 line of code)
+2.) Print the count of 1 through 6. (For example, "There are 1361 amount of 2s") (3 lines of code)
+3.) Print the average of all 10,000 random numbers. (Make sure it's a float) (2 lines of code)
 '''
 
 
