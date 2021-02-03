635b925358d57a57df121074f2ea0daa203e9e75
Marc Hermon <hermonm@urbandaleschools.com>
2021-02-03 09:18:17 -0600

    Update and rename 9.1_Min_Function.py to 9.1_Mini_Function.py

--- 9.1_Mini_Function.py
+++ 9.1_Mini_Function.py
@@ -1,18 +1,18 @@
 '''
-MIN FUNCTION
+MINI FUNCTION
 ------------
-Write a function called min that will take three numbers as parameters 
+Write a function called mini that will take three numbers as parameters 
 and return the smallest value. If more than one number is tied for smallest, 
 still return that smallest number. Once you've finished writing your function, 
 copy/paste the following code and make sure that it runs correctly with the function you created:
 
 INPUT
 -----
-print(min(7, 3, 5))
-print(min(5, 5, 4))
-print(min(2, 2, 3))
-print(min(-2, -6, -100))
-print(min("Z", "B", "A"))
+print(mini(7, 3, 5))
+print(mini(5, 5, 4))
+print(mini(2, 2, 3))
+print(mini(-2, -6, -100))
+print(mini("Z", "B", "A"))
 
 OUTPUT
 ------
